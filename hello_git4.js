0cdb234e39de4a097d3b78daa2dc0588f007fad7
ruser <sergiotr22@gmail.com>
2025-03-31 20:34:54 -0400

    cambios en hello_git4.js, nuevos mensajes por consola

--- hello_git4.js
+++ hello_git4.js
@@ -1,1 +1,4 @@
 console.log("prompt es un modulo de nodeJS");
+console.log("Nodejs es magnifico");
+console.log("Nodejs tiene muchos modulos utiles");
+console.log("fs es otro modulo de node como el modulo prompt");
